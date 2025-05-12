265b07d44c101faf2e85ded37afc52eaef8190df
Jeremie BAYON <jeremie.bayon@careside.care>
2025-05-12 18:16:05 +0200

    Add some more functionalities for reservation

--- models/Reservation.js
+++ models/Reservation.js
@@ -1,22 +1,52 @@
 const pool = require('../config/db.js');
-let rows = [];
 
 async function getAllReservations() {
-    return rows = await pool.query('SELECT * FROM reservations');
+    const [rows] = await pool.query('SELECT * FROM reservations');
+    return rows
 }
 
 async function getAllReservationsById(userId) {
-    return rows = await pool.query('SELECT * FROM reservations WHERE user_id = ?', userId);
+    const [rows] = await pool.query('SELECT * FROM reservations WHERE user_id = ?', userId);
+    return rows
 }
 
-async function AddReservation(userId, numberPeople, date, statusId) {
-    return rows = await pool.query(`INSERT INTO reservations (id, user_id, number_people, date, status_id) VALUES (${userId}, ${numberPeople}, ${date}, ${statusId})`);
+async function getReservationById(reservationId) {
+    const [rows] = await pool.query('SELECT * FROM reservations WHERE id = ?', reservationId);
+    return rows
+}
+
+async function addReservation(userId, numberPeople, date, statusId) {
+    const [rows] = await pool.query(`INSERT INTO reservations (id, user_id, number_people, date, status_id) VALUES (${userId}, ${numberPeople}, ${date}, ${statusId})`);
+    return rows
+}
+
+async function updateReservation(userId, numberPeople, date, statusId, reservationId) {
+    const [rows] = await pool.query(`UPDATE reservation SET user_id = ${userId}, numberPeople = ${numberPeople}, date = ${date}, status_id = ${statusId} WHERE id = ${reservationId})`);
+    return rows
+}
+
+async function deleteReservation(reservationId) {
+    const statusId = 3;
+    const [rows] = await pool.query(`UPDATE reservation SET status_id = ${statusId} WHERE id = ${reservationId})`);
+    return rows
+}
+
+async function validateReservation(reservationId) {
+    const statusId = 2;
+    const [rows] = await pool.query(`UPDATE reservation SET status_id = ${statusId} WHERE id = ${reservationId})`);
+    return rows
 }
 
 module.exports = {
     getAllReservations,
     getAllReservationsById,
-};
+    getReservationById,
+    addReservation,
+    updateReservation,
+    deleteReservation,
+    validateReservation,
+}
+
 
 
 
--- routes/reservationRoutes.js
+++ routes/reservationRoutes.js
@@ -3,6 +3,11 @@ const controllers = require('../controllers/reservationController.js');
 function initReservations(app) {
     app.get('/reservations', controllers.getAllReservations)
     app.get('/my-reservations', controllers.getAllReservationsById)
+    app.get('/my-reservation', controllers.getReservationById)
+    app.post('/reservations', controllers.addReservation)
+    app.put('/reservations/:id', controllers.updateReservation)
+    app.delete('/reservations/:id', controllers.updateReservation)
+    app.patch('/reservations/:id/validate', controllers.validateReservation)
 }
 
 module.exports = initReservations;
