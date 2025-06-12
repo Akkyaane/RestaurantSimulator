3fc26fde127d0559c4d1304c9e8bf77184f1256c
Crtmetton <crtmetton@gmail.com>
2025-06-12 11:55:52 +0200

    Order By for Menu

--- models/MenuItem.js
+++ models/MenuItem.js
@@ -3,7 +3,7 @@ const db = require('../config/db');
 
 const getAllMenuItems = async () => {
   try {
-    const [results] = await db.query('SELECT * FROM menu_items');
+    const [results] = await db.query('SELECT * FROM menu_items order by category_id');
     return results;
   } catch (err) {
     throw new Error(err);
